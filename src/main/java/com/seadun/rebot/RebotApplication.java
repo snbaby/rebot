2b49406bed80bfa33b1c519464f3c05cc0cb6728
wangjian <wangj@seadun.com>
2018-03-14 17:47:17 +0800

    新增项目

---
+++ src/main/java/com/seadun/rebot/RebotApplication.java
@@ -0,0 +1,12 @@
+package com.seadun.rebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebotApplication.class, args);
+	}
+}
