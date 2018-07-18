6f3a6a73a80b82325c3a8b495d1062449e548b35
sn_baby <sn_baby@qq.com>
2018-07-18 13:21:43 +0800

    修改

--- src/main/java/com/seadun/rebot/rest/DictController.java
+++ src/main/java/com/seadun/rebot/rest/DictController.java
@@ -2,13 +2,10 @@ package com.seadun.rebot.rest;
 
 import java.util.UUID;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.seadun.rebot.entity.DictMemoryType;
